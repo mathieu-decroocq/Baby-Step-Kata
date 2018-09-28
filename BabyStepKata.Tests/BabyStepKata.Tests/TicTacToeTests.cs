1baec234aee93ca102aab1901db750d5751d15e0
mathieu-decroocq-econocom <mathieu.decroocq@econocom.com>
2018-09-28 13:54:29 +0200

    init vs project

---
+++ BabyStepKata.Tests/BabyStepKata.Tests/TicTacToeTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BabyStepKata.Tests
+{
+    public class TicTacToeTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
